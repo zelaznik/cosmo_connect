5391eea2aefdce890add6d9d7d35f8c31f731163
Steve Zelaznik <zelaznik@yahoo.com>
2015-08-14 12:06:41 -0700

    Finally got that damn subview to work.  Thanks Jade.

--- app/assets/javascripts/views/user/response_index.js
+++ app/assets/javascripts/views/user/response_index.js
@@ -1,9 +1,18 @@
 var Cosmo = window.Cosmo;
 
 Cosmo.Views.ResponseIndex = Backbone.CompositeView.extend({
+  template: JST['essays/index'],
+
+  render: function() {
+    var content = this.template({});
+    this.$el.html(content);
+
+    return this;
+  },
+
   initialize: function () {
     this.listenTo(this.collection, 'add', this.addSubView);
-    this.listenTo(this.collection, 'remove', this.removeResponse);
+    this.listenTo(this.collection, 'remove', this.removeResponse.bind(this));
     this.listenTo(this.collection, 'add remove', this.render);
   },
 
--- app/assets/javascripts/views/user/user_show.js
+++ app/assets/javascripts/views/user/user_show.js
@@ -2,9 +2,17 @@ var Cosmo = window.Cosmo;
 
 Cosmo.Views.UserShow = Backbone.CompositeView.extend({
   template: JST['users/show'],
+  model: Cosmo.Models.User,
 
   initialize: function() {
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(this.model.responses(), 'sync', this.render);
+
+    var responseIndexView = new Cosmo.Views.ResponseIndex({
+      collection: this.model.responses()
+    });
+
+    this.addSubview('.response-index', responseIndexView);
   },
 
   render: function () {
