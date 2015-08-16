b62cf5aa3b4573cb4ce2d12f3e03d85b5c6c04c8
Steve Zelaznik <zelaznik@yahoo.com>
2015-08-16 10:22:12 -0700

    Successfully posts to the database.  Now we have some unnecessary GET requests to get rid of.

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -1,11 +1,27 @@
 class Api::ResponsesController < ApplicationController
+
+  def new
+    @response = Response.new(response_params)
+    @response.user = current_user
+    if @response.save
+      render json: {}
+    else
+      render json: @response.errors.full_messages
+    end
+  end
+
   def update
-    raise "Zelaznik is awesome!"
+    @response = Response.find(params[:id])
+    if @response.update(response_params)
+      render json: {}
+    else
+      render json: @response.errors.full_messages
+    end
   end
 
   private
   def response_params
-    params.require(:response).permit()
+    params.require(:response).permit(:id, :response_category_id, :body)
   end
 
 end
