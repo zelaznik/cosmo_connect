00d063fbc0fb1aa1eaa6f6a1ff02c2eb93db928b
Steve Zelaznik <zelaznik@yahoo.com>
2015-08-18 16:56:31 -0700

    Successfully got UsersIndex view to work (i.e. browse profiles)

--- app/assets/javascripts/views/user/user_index_item.js
+++ app/assets/javascripts/views/user/user_index_item.js
@@ -1,13 +1,13 @@
 var Cosmo = window.Cosmo;
 
 Cosmo.Views.UserIndexItem = Backbone.View.extend({
-  template: JST['users/show'],
-  className: 'user-index-item',
+  template: JST['users/preview'],
+  className: 'row user-index-item',
   initialize: function(options) {
   },
   render: function() {
     var content = this.template({
-
+      user: this.model
     });
     this.$el.html(content);
     return this;
