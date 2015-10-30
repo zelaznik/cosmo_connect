48f7b5f19b92e317ddd88d7fbf1c6542cb070b24
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-30 02:05:01 -0700

    More work on composite views.

--- app/assets/javascripts/views/details/details_index.js
+++ app/assets/javascripts/views/details/details_index.js
@@ -1,10 +1,33 @@
 var Cosmo = window.Cosmo;
 
 Cosmo.Views.DetailsIndex = Backbone.CompositeView.extend({
-  template: JST['users/details'],
+  template: JST['details/index'],
+
+  initialize: function(options) {
+    this.listenTo(this.collection, 'add', this.addDetailsSubview);
+    this.listenTo(this.collection, 'remove', this.removeDetailsSubview);
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  lookup: {
+    'birthdate': {'title': 'Date Of Birth'}
+  },
+
+  addDetailsSubview: function(detail) {
+    var messageIndexItem = new Cosmo.Views.DetailsIndexItem({
+      collection: this.collection,
+      model: detail
+    });
+    this.addSubview('#message-index-results', messageIndexItem);
+  },
+
+  removeDetailsSubview: function(model) {
+    this.removeModelSubView('.message-index', model);
+  },
 
   render: function () {
     this.$el.html(this.template({
+      details: this.model.details,
       user: this.model
     }));
     this.attachSubviews();
