5e63f1620d1169c0631f970dd8c8e462fce669d2
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-01 15:42:04 -0800

    Removed debugger from details height controller.

--- app/controllers/api/details_controller.rb
+++ app/controllers/api/details_controller.rb
@@ -72,7 +72,6 @@ class Api::DetailsController < Api::BaseController
     elsif k == 'height'
       model = current_user.details
       height = 12 * v[:feet].to_i + v[:inches].to_i
-      debugger
       update_params = {height: height}
       output_params = {
         id: :height,
