505398bbd7aaee387ea72a631b6d1d285d4a4dbe
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-03 18:33:16 -0800

    Enforce username lowercase.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
+  before_save { |user| user.name = user.name.downcase }
   attr_reader :password
 
   def self.find_by_credentials(username, password)
